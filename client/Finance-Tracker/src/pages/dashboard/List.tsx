fcc44f7b8a13d09872a4a83df1a02b1d7e34624e
MrJayMac <mac.joshua450@gmail.com>
2024-06-25 17:30:14 -0400

    creating react tables in list component

--- client/Finance-Tracker/src/pages/dashboard/List.tsx
+++ client/Finance-Tracker/src/pages/dashboard/List.tsx
@@ -1,6 +1,39 @@
+import { useFinancialRecords } from "../../context/context"
+import { useTable, Column, CellProps, Row } from "react-table";
+
+
 function list() {
+
+  const {records} = useFinancialRecords();
+
+  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = 
+    useTable ({
+      columns, 
+      data: records
+    });
+
   return (
-    <div>list</div>
+    <div className="table-container">
+      <table {...getTableProps()} className="table">
+        <thead>
+          {headerGroups.map((hg) => (
+            <tr {...hg.getHeaderGroupProps}>
+              {hg.headers.map((column) => (
+                <th {...column.getHeaderProps()}> {column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, idx) =>{
+            prepareRow(row);
+            return <tr {...row.getRowProps()}> {row.cells.map((cell) => (
+              <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+            ))}</tr>
+          })}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
